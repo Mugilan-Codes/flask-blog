41ea1c9ecbb9a557683f955af84e7e0483bccf2d
Mugilan <mugilancodes@gmail.com>
2020-10-18 18:49:55 +0530

    Dummy Data

--- flaskblog.py
+++ flaskblog.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Mugilan E.S.",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "April 23, 1999",
+    },
+    {
+        "author": "Samantha",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "October 18, 2020",
+    },
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
