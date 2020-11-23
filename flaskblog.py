cae85062bce47a9123fb82ad91e60a2f74e78f35
Mugilan <mugilancodes@gmail.com>
2020-11-23 21:26:44 +0530

    Auth Page

--- flaskblog.py
+++ flaskblog.py
@@ -43,9 +43,15 @@ def register():
     return render_template("register.html", title="Register Form", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash(f"You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Failed!, Please check your username and password", "danger")
     return render_template("login.html", title="Login Form", form=form)
 
 
