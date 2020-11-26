c9ef62a5d16924b22f022f4bd8a09f48045669b5
Mugilan <mugilancodes@gmail.com>
2020-11-26 13:31:41 +0530

    Login User

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # to disable deprecation warning
@@ -12,5 +13,6 @@ app.config["SECRET_KEY"] = "ecc3c74cde310eb6fc1d801b266aa3c8"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
